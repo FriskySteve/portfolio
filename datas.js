8fdac6ffbe819ae73daaec4ff340c06d1e6de027
Michal Stasiak <michal.stasiak@protonmail.com>
2024-11-26 22:07:23 +0100

    remodel my scheme

---
+++ datas.js
@@ -0,0 +1,29 @@
+const datas = [
+  {
+    link: "HOME",
+    title: "JAN KOWALSKI",
+    desc: "WEB-DESIGNER",
+  },
+  {
+    link: "PROJECTS",
+    title: "MY PROJECTS",
+    desc: "MADE WITH LOVE",
+  },
+  {
+    link: "ABOUT",
+    title: "ABOUT ME",
+    desc: "IT’S A-ME, JAN!",
+  },
+  {
+    link: "CONTACT",
+    title: "CONTACT ME",
+    desc: "SAY HELLO TO ME",
+  },
+  {
+    link: "MESSAGES",
+    title: "MESSAGES",
+    desc: "MESSAGE FROM THE INTERESTED PERSON",
+  },
+];
+
+export { datas };
---
+++ script.js
@@ -0,0 +1,5 @@
+import { datas } from "./datas.js";
+// console.log(datas[0].title);
+const test = document.getElementById("navbar");
+// test.textContent = datas[0].title;
+console.log(datas[0].title);
