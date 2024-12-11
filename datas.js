215db62ff62aec4ccedc33d4843b484d2393074e
Michal Stasiak <michal.stasiak@protonmail.com>
2024-12-11 18:38:27 +0100

    change conception, restart project, done home page

--- datas.js
+++ datas.js
@@ -1,29 +1,124 @@
-const datas = [
+const data = {
+  links: ["home", "projects", "about", "contact", "messages"],
+  logo: {
+    golden: "ITP",
+    silver: "ortfolio",
+    copyright: " © 2024",
+  },
+  headerTitle: {
+    home: {
+      title: "Jan Kowalski",
+      p: "WEB-DESIGNER",
+    },
+    projects: {
+      title: "My Projects",
+      p: "MADE WITH LOVE",
+    },
+    about: {
+      title: "About Me",
+      p: "IT’S A-ME, JAN!",
+    },
+    contact: {
+      title: "Contact Me",
+      p: "SAY HELLO TO ME",
+    },
+    messages: {
+      title: "Messages",
+      p: "MESSAGE FROM THE INTERESTED PERSON",
+    },
+  },
+  footer: {
+    email: "jan_kowalski@gmail.com",
+    phone: "+123 456 789",
+  },
+};
+
+// const datas = [
+//   {
+//     link: "HOME",
+//     title: "JAN KOWALSKI",
+//     desc: "WEB-DESIGNER",
+//   },
+//   {
+//     link: "PROJECTS",
+//     title: "MY PROJECTS",
+//     desc: "MADE WITH LOVE",
+//   },
+//   {
+//     link: "ABOUT",
+//     title: "ABOUT ME",
+//     desc: "IT’S A-ME, JAN!",
+//   },
+//   {
+//     link: "CONTACT",
+//     title: "CONTACT ME",
+//     desc: "SAY HELLO TO ME",
+//   },
+//   {
+//     link: "MESSAGES",
+//     title: "MESSAGES",
+//     desc: "MESSAGE FROM THE INTERESTED PERSON",
+//   },
+// ];
+
+const skills = [
+  { name: "HTML", img: "img/html.png" },
   {
-    link: "HOME",
-    title: "JAN KOWALSKI",
-    desc: "WEB-DESIGNER",
+    name: "CSS",
+    img: "img/css.png",
+    exp: "3 years",
   },
   {
-    link: "PROJECTS",
-    title: "MY PROJECTS",
-    desc: "MADE WITH LOVE",
+    name: "JavaScript",
+    img: "img/js.png",
+    exp: "3 years",
   },
   {
-    link: "ABOUT",
-    title: "ABOUT ME",
-    desc: "IT’S A-ME, JAN!",
+    name: "Git",
+    img: "img/git.png",
+    exp: "3 years",
   },
   {
-    link: "CONTACT",
-    title: "CONTACT ME",
-    desc: "SAY HELLO TO ME",
+    name: "Figma",
+    img: "img/figma.png",
+    exp: "3 years",
   },
   {
-    link: "MESSAGES",
-    title: "MESSAGES",
-    desc: "MESSAGE FROM THE INTERESTED PERSON",
+    name: "Chrome",
+    img: "img/chrome/png",
+    exp: "3 years",
+  },
+  {
+    name: "VSCode",
+    img: "img/vsc.png",
+    exp: "3 years",
+  },
+  {
+    name: "GitHub",
+    img: "img/github.png",
+    exp: "3 years",
   },
 ];
 
-export { datas };
+const background = [
+  "My background",
+  [
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ",
+    "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    "At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ",
+    "At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla",
+  ],
+];
+
+const hobbies = [
+  "My hobbies and interests",
+  [
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ",
+    "At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ",
+    "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla",
+  ],
+];
+
+export { data, skills, background, hobbies };
--- script.js
+++ script.js
@@ -1,5 +1,138 @@
-import { datas } from "./datas.js";
-// console.log(datas[0].title);
-const test = document.getElementById("navbar");
-// test.textContent = datas[0].title;
-console.log(datas[0].title);
+import { data, skills, background, hobbies } from "./datas.js";
+
+let active = "HOME";
+
+loadList(data.links, "header-links");
+loadLogo("header-logo", true);
+loadTitleAndP(data.headerTitle.about, "title-container");
+loadFooter();
+
+function loadList(source, destination) {
+  source.forEach((element) => {
+    const $ul = document.getElementById(destination);
+    const li = document.createElement("li");
+    li.textContent = element;
+    $ul.appendChild(li);
+  });
+}
+
+function loadLogo(destination, header) {
+  const $destination = document.getElementById(destination);
+  const goldenLogo = document.createElement("span");
+  const silverLogo = document.createElement("span");
+  const whiteLogo = document.createElement("span");
+  goldenLogo.textContent = data.logo.golden;
+  goldenLogo.classList.add("golden-logo");
+  silverLogo.textContent = data.logo.silver;
+  silverLogo.classList.add("silver-logo");
+  whiteLogo.textContent = data.logo.copyright;
+  whiteLogo.classList.add("white-logo");
+  $destination.append(goldenLogo, silverLogo);
+  if (header) {
+    silverLogo.id = "logo-to-hide";
+  } else {
+    $destination.append(whiteLogo);
+  }
+}
+
+function loadTitleAndP(source, destination) {
+  const $title = document.createElement("h1");
+  const $p = document.createElement("p");
+  const $destination = document.getElementById(destination);
+  $title.textContent = source.title;
+  $p.textContent = source.p;
+  $destination.append($title, $p);
+}
+
+function loadFooter() {
+  loadList(data.links, "footer-links");
+
+  const $email = document.createElement("p");
+  const $phone = document.createElement("p");
+  const $copyrights = document.getElementById("copyright");
+  $email.textContent = data.footer.email;
+  $phone.textContent = data.footer.phone;
+  $copyrights.append($email, $phone);
+  loadLogo("copyright", false);
+}
+
+// const loadLinkList = () => {
+//   const $ulLinks = document.querySelectorAll(".links");
+//   datas.forEach((data) => {
+//     const link = document.createElement("li");
+//     link.textContent = data.link;
+//     $ulLinks.forEach(($ul) => {
+//       $ul.appendChild(link.cloneNode(true));
+//     });
+//   });
+// };
+
+// function loadTitle() {
+//   const titleContainer = document.getElementById("title-container");
+//   const title = document.createElement("h1");
+//   title.textContent = datas[0].title;
+//   const desc = document.createElement("h2");
+//   desc.textContent = datas[0].desc;
+//   titleContainer.append(title, desc);
+// }
+
+// loadLinkList();
+// loadTitle();
+
+// switch (active) {
+//   case "home":
+//     home();
+//     break;
+//   case "projects":
+//     projects();
+//     break;
+//   case "about":
+//     about();
+//     break;
+//   case "contact":
+//     contact();
+//     break;
+//   case "messages":
+//     messages();
+//     break;
+
+//   default:
+//     home();
+//     break;
+// }
+
+// function home() {
+//   articleCreator();
+// }
+// function projects() {}
+// function about() {}
+// function contact() {}
+// function messages() {}
+// function skillCreator() {
+//   const title = document.createElement("h4");
+//   const skillsHolder = document.createElement("div");
+//   title.textContent = "My Skills";
+//   skills.forEach((skill) => {
+//     const skillHolder = document.createElement("div");
+//     const img = document.createElement("img");
+//     const skillName = document.createElement("h4");
+//     const ul = document.createElement("ul");
+//     const exp = document.createElement("p");
+//     img.src = skill.img;
+//     skillName.textContent = skill.name;
+//     exp.textContent = skill.exp;
+//     skillHolder.append(img, skillName, ul, exp);
+//     skillsHolder.appendChild(skillHolder);
+//   });
+// }
+
+// function articleCreator(title, text, destination) {
+//   const title = document.createElement("h2");
+//   title.textContent = title;
+//   destination.append(title);
+//   text.forEach((paragrah) => {
+//     const param = document.createElement("p");
+//     param.textContent = paragrah;
+//     destination.append(param);
+//   });
+// }
